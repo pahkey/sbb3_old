71bc0e2631e73ae4800f7ae7b089291c4d6441ee
박응용 <pahkey@gmail.com>
2022-12-12 13:50:23 +0900

    2-02

---
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -0,0 +1,15 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index() {
+    	return "안녕하세요 sbb에 오신것을 환영합니다.";
+    }
+}
