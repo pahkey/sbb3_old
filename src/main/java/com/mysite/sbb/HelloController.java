f9e86e9b843af528bb638f0d4aa2f3db2d4fe977
박응용 <pahkey@gmail.com>
2022-12-12 13:40:39 +0900

    1-04

---
+++ src/main/java/com/mysite/sbb/HelloController.java
@@ -0,0 +1,14 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World";
+    }
+}
