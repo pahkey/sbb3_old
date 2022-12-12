8f835ad56639f2381f3abd4cb551721aac29ded8
박응용 <pahkey@gmail.com>
2022-12-12 19:10:44 +0900

    2-11

---
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -0,0 +1,27 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
+        Question question = this.questionService.getQuestion(id);
+        this.answerService.create(question, content);
+        return String.format("redirect:/question/detail/%s", id);
+    }
+}
---
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -0,0 +1,23 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+
+    private final AnswerRepository answerRepository;
+
+
+    public void create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+    }
+}
