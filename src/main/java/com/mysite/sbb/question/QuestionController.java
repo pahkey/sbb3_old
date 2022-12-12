6f0529c0260c8ef07afa9a3b56cc6093d09f4412
박응용 <pahkey@gmail.com>
2022-12-12 16:23:11 +0900

    2-09

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -12,11 +12,11 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+	private final QuestionService questionService;
 
     @GetMapping("/question/list")
     public String list(Model model) {
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
---
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -0,0 +1,18 @@
+package com.mysite.sbb.question;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
+}
