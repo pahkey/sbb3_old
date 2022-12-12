d46b6148865fc38021ee3e38e1b51f5a8d3b581a
박응용 <pahkey@gmail.com>
2022-12-12 16:19:18 +0900

    2-07

---
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -0,0 +1,23 @@
+package com.mysite.sbb.question;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+
+    private final QuestionRepository questionRepository;
+
+    @GetMapping("/question/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
+}
