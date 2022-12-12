c5aaa4de07fbe8b87484128c0452e89d70488793
박응용 <pahkey@gmail.com>
2022-12-13 08:58:22 +0900

    3-05

---
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.mysite.sbb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().requestMatchers(
+                new AntPathRequestMatcher("/**")).permitAll()
+        	.and()
+        		.csrf().ignoringRequestMatchers(
+        				new AntPathRequestMatcher("/h2-console/**"))
+        	.and()
+                .headers()
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+        		;
+        return http.build();
+    }
+}
