7905651d0ac25b1b14a1bcce706a26c7bc186567
박응용 <pahkey@gmail.com>
2022-12-12 13:47:46 +0900

    1-05

--- src/main/java/com/mysite/sbb/HelloController.java
+++ src/main/java/com/mysite/sbb/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
     @GetMapping("/hello")
     @ResponseBody
     public String hello() {
-        return "Hello World";
+    	return "Hello Spring Boot Board";
     }
 }
---
+++ src/main/java/com/mysite/sbb/HelloLombok.java
@@ -0,0 +1,18 @@
+package com.mysite.sbb;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class HelloLombok {
+
+    private final String hello;
+    private final int lombok;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok("헬로", 5);
+        System.out.println(helloLombok.getHello());
+        System.out.println(helloLombok.getLombok());
+    }
+}
