2fd12727f9e099f392b3f68bf60854cd870fa7f8
박응용 <pahkey@gmail.com>
2022-12-12 13:31:40 +0900

    sbb3 init

---
+++ src/main/java/com/mysite/sbb/SbbApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
