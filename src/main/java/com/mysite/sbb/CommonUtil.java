dc353c508668237e8a6251aa6fe2f058577aaf98
박응용 <pahkey@gmail.com>
2022-12-13 11:52:42 +0900

    3-13

---
+++ src/main/java/com/mysite/sbb/CommonUtil.java
@@ -0,0 +1,16 @@
+package com.mysite.sbb;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonUtil {
+    public String markdown(String markdown) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
+    }
+}
